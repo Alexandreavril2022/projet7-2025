5a7b894e34b1885d7d04d7f6eaac81c26bc13bb3
Alexandreavril2022 <104219828+Alexandreavril2022@users.noreply.github.com>
2025-08-04 10:05:46 +0200

    Add files via upload

---
+++ api.py
@@ -0,0 +1,28 @@
+import pickle
+from flask import request, jsonify, Flask
+import pandas as pd
+
+
+app = Flask(__name__)
+
+filename = "lr_model.pkl"
+model = pickle.load(open(filename, 'rb'))
+
+@app.route("/")
+def main_page():
+    return "<p>Loan Prediction API !</p>"
+
+@app.route('/predict', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        data = request.json
+        sample = pd.DataFrame(data, index=[0])
+        prediction = model.predict(sample)
+        # print("PREDICTION", prediction)
+        response = jsonify({"class": str(prediction[0])})
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
