346830db36a9da7d7a1cfc7b454247768d199e47
Alexandreavril2022 <104219828+Alexandreavril2022@users.noreply.github.com>
2025-08-04 10:45:37 +0200

    Update api.py

--- api.py
+++ api.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 filename = "lr_model.pkl"
 model = pickle.load(open(filename, 'rb'))
 
-@app.route("/",methods=['GET'])
+@app.route("/")
 def main_page():
     return "<p>Loan Prediction API !</p>"
 
